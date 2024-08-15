496ad67db3e844729966f000a9ff40f206b7db0a
Al Cerdan Lico <alcerdan@yahoo.com>
2024-08-14 18:21:07 -0700

    completed Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = (["Charlie", "Samip", "Ali"], "surprise");
+
+function writeCards(cards, surprise) {
+    const messages  = []
+  for (let i = 0; i < cards.length; i++) {
+    messages.push(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
+
+  };
+
+  return messages;
+
+}
+
+
+function countDown() {
+
+    for (let countDown = 10; countDown >-1; countDown--) {
+        console.log(countDown);
+    }
+    
+    }
