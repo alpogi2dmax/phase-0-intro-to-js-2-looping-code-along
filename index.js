29c5c68a509869b1a6c610e747069e4908d14438
Al Cerdan Lico <alcerdan@yahoo.com>
2024-09-10 12:33:28 -0700

    completed looping-code-along

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const cards = (["Charlie", "Samip", "Ali"], "surprise");
+const friends = ["Guadalupe", "Ollie", "Aki"];
+const occassion = "surprise";
 
-function writeCards(cards, surprise) {
-    const messages  = []
-  for (let i = 0; i < cards.length; i++) {
-    messages.push(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
-
+function writeCards(friends, occassion) {
+  let messages = [];
+  for (let i = 0; i< friends.length; i++) {
+    messages.push(`Thank you, ${friends[i]}, for the wonderful ${occassion} gift!`);
   };
 
   return messages;
-
 }
 
-
 function countDown() {
-
-    for (let countDown = 10; countDown >-1; countDown--) {
-        console.log(countDown);
-    }
-    
-    }
+  for (let count = 10; count >-1; count--) {
+    console.log(count);
+  }
+}
